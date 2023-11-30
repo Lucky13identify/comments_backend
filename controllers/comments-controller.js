9ab171137a24fa9f921a822f8732dd6a706a79fb
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-11-30 12:21:12 +0200

    added pagination

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -3,7 +3,25 @@ const db = require("../config/db");
 const comment = require("../models/comments");
 
 const getComments = async (req, res) => {
-  const sql = "SELECT * FROM comments";
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = parseInt(req.query.pageSize) || 25;
+
+  const startIndex = (page - 1) * pageSize;
+
+  if (!req.query.page || !req.query.pageSize) {
+    const sqlAllComments = "SELECT * FROM comments";
+    db.query(sqlAllComments, function (err, result) {
+      if (err) {
+        res.status(500).json({ message: "Ошибка выполнения GET-запроса" });
+        return;
+      }
+
+      res.status(200).json({ comments: result });
+    });
+    return;
+  }
+
+  const sql = `SELECT * FROM comments LIMIT ${pageSize} OFFSET ${startIndex}`;
 
   db.query(sql, function (err, result) {
     if (err) {
@@ -11,7 +29,12 @@ const getComments = async (req, res) => {
       return;
     }
 
-    res.status(200).json({ comments: result });
+    res.status(200).json({
+      comments: result,
+      page,
+      pageSize,
+      totalComments: result.length,
+    });
   });
 };
 
@@ -20,7 +43,7 @@ const postComment = async (req, res) => {
 
   await comment.validateAsync({ user, email, homePage, text });
 
-  const sql = `INSERT INTO comments (user, email, homePage, text) VALUES ('${user}', '${email}', '${homePage}', '${text}')`;
+  const sql = `INSERT INTO comments (user, email, homePage, text, date) VALUES ('${user}', '${email}', '${homePage}', '${text}', '${new Date()}')`;
 
   try {
     await db.query(sql, function (err) {
